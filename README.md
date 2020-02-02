a57624a07acf9af79813fcd0085f56d6c6f70490
Ankit.K <ankit.k@hashedin.com>
2020-02-02 18:33:45 +0530

    1. adding static files for now till i segreggate the UI in S3 ; 2. .gitignore :=> ignored __pycache__ ; 3. app.py :=> i) added code for redering html and static js and css files , 4. added static/ and template/ directory

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MarketingManager
 This an application for Marketing Company for managing their Customer relations  
+
+imp. links :-
+https://exploreflask.com/en/latest/organizing.html
+https://www.tutorialspoint.com/flask/flask_static_files.htm
+https://www.codementor.io/@johnfemi/how-to-build-restful-apis-with-python-and-flask-fh5x7zjrx
+https://flask-restful.readthedocs.io/en/latest/quickstart.html
+https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_restful import Resource, Api, request
 
 app = Flask(__name__)
+api = Api(app)
 
 
 @app.route('/')
 def index():
-    return "index template"
+    return render_template('index.html', name="ankit")
 
+
+class SimpleApi(Resource):
+    def get(self):
+        return {"message": "successful GET api"}
+
+
+class SimplePostApi(Resource):
+    def post(self, id):
+        query_params = dict(request.args)
+        print(query_params)
+        return {"message": "successful POST api with id : {}".format(id)}
+
+
+api.add_resource(SimpleApi, '/api/simple-api')
+
+api.add_resource(SimplePostApi, '/api/simple-post-api/<int:id>')
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=4000)
